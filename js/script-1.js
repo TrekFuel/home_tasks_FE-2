b299c18685632797cb26cdfd2ad7fffbc07a94c8
TrekFuel <ridge96@yandex.ru>
2019-12-06 23:24:50 +0300

    fix(project): fix script-2.js, script-3.js

--- js/script-1.js
+++ js/script-1.js
@@ -1,28 +1,30 @@
 function getNumWord(num, word1, word2, word5) {
-    let dd = num % 100;
+  const dd = num % 100;
 
-    if ((dd >= 11) && (dd <= 19)) {
-        return word5;
-    }
+  if ((dd >= 11) && (dd <= 19)) {
+    return word5;
+  }
 
-    let oneDigit = num % 10;
-    switch (true) {
-        case (oneDigit === 1):
-            return word1;
-        case (oneDigit >= 2 && oneDigit <= 4):
-            return word2;
-        default:
-            return word5;
-    }
+  const oneDigit = num % 10;
+  switch (true) {
+    case (oneDigit === 1):
+      return word1;
+    case (oneDigit >= 2 && oneDigit <= 4):
+      return word2;
+    default:
+      return word5;
+  }
 }
 
 
 function makeTest() {
-    let applesCount = parseInt(prompt('Сколько яблок?'));
-    if (applesCount) {
-        alert('У вас' + ' ' + applesCount + ' ' + getNumWord(applesCount,
-            'яблоко', 'яблока', 'яблок'));
-    }
+  // eslint-disable-next-line radix,no-alert
+  const applesCount = parseInt(prompt('Сколько яблок?'));
+  if (applesCount) {
+    // eslint-disable-next-line no-alert,no-useless-concat
+    alert(`${'У вас' + ' '}${applesCount} ${getNumWord(applesCount,
+      'яблоко', 'яблока', 'яблок')}`);
+  }
 }
 
 makeTest();
--- js/script-2.js
+++ js/script-2.js
@@ -1,22 +1,36 @@
+// eslint-disable-next-line consistent-return
 function getUserData() {
-    let lastName = prompt('Введите Вашу фамилию');
-    let firstName = prompt('Введите Ваше имя');
-    let middleName = prompt('Введите Ваше отчество');
-    if (lastName === null || firstName === null || middleName === null || lastName === '' || firstName === '' || middleName === '') {
-        return getUserData(alert('Введите ФИО корректно, пожалуйста'))
-    }
-    let age = +(prompt('Введите Ваш возраст'));
-    if (age == null || age == '' || age !== +age) {
-        return getUserData(alert('Введите Ваш возраст корректно, пожалуйста'))
-    }
-    let gender = confirm('Ваш пол-мужской?');
-    let isRetired = confirm('Вы на пенсии?');
-    alert('Ваша ФИО: ' + lastName + ' ' + firstName + ' ' + middleName + ';' + ' ' + 'Ваш' +
-        ' возраст:' + ' ' + age + ';' + ' ' + 'Ваш возраст в днях: ' + `${age * 365}` + ';' + ' ' + 'Через 5 лет Вам будет: ' + `${+(age) + 5}` + ';' + ' ' + 'Ваш' +
-        ' пол:' + ' ' + gender + ';' + ' ' + 'Вы' +
-        ' на' +
-        ' пенсии:' + ' ' + isRetired + '.')
-    ;
+  // eslint-disable-next-line no-alert
+  const lastName = prompt('Введите Вашу фамилию');
+  // eslint-disable-next-line no-alert
+  const firstName = prompt('Введите Ваше имя');
+  // eslint-disable-next-line no-alert
+  const middleName = prompt('Введите Ваше отчество');
+  if (lastName === null || firstName === null || middleName === null || lastName === '' || firstName === '' || middleName === '') {
+    // eslint-disable-next-line no-alert
+    alert('Введите ФИО корректно, пожалуйста');
+    return getUserData();
+  }
+  // eslint-disable-next-line no-alert
+  const age = +(prompt('Введите Ваш возраст'));
+  if (age == null || age == '' || age !== +age) {
+    // eslint-disable-next-line no-alert
+    alert('Введите Ваш возраст корректно, пожалуйста');
+    return getUserData();
+  }
+  // eslint-disable-next-line no-restricted-globals,no-alert
+  const gender = confirm('Ваш пол-мужской?');
+  // eslint-disable-next-line no-restricted-globals,no-alert
+  const isRetired = confirm('Вы на пенсии?');
+  // eslint-disable-next-line no-alert,no-useless-concat
+  alert(`Ваша ФИО: ${lastName} ${firstName} ${middleName};` + ' ' + 'Ваш'
+      // eslint-disable-next-line no-useless-concat
+      + ' возраст:' + ` ${age};` + ' ' + 'Ваш возраст в днях: ' + `${age * 365}` + ';' + ' ' + 'Через 5 лет Вам будет: ' + `${+(age) + 5}` + ';' + ' ' + 'Ваш'
+      // eslint-disable-next-line no-useless-concat
+      + ' пол:' + ` ${gender};` + ' ' + 'Вы'
+      + ' на'
+      // eslint-disable-next-line no-useless-concat
+      + ' пенсии:' + ` ${isRetired}.`);
 }
 
 getUserData();
