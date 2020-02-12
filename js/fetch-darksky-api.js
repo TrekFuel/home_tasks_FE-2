9a47f7b31b3ab268470709c2b1572a339b2f17fb
TrekFuel <ridge96@yandex.ru>
2020-02-12 21:06:46 +0300

    fix(project): fix hw-6, hw-7

--- js/fetch-darksky-api.js
+++ js/fetch-darksky-api.js
@@ -2,16 +2,21 @@ const getButton = document.getElementById('getButton');
 const renderElement = document.getElementById('container');
 
 function getGrodnoForecast() {
-  fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/9ac094ddbff4dcbaf603076dfbe3874c/53.683802,23.833985,2020-02-05T15:00:00?lang=ru&units=si', {
-    method: 'GET',
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      // eslint-disable-next-line no-undef
-      const unixDate = moment.unix(response.currently.time);
-      const humanDate = unixDate.locale('ru').format('MMMM Do YYYY, h:mm:ss a');
+    const proxy = 'https://cors-anywhere.herokuapp.com';
+    const API = 'https://api.darksky.net/forecast';
+    const key = '9ac094ddbff4dcbaf603076dfbe3874c';
+    const params = '53.683802,23.833985,2020-02-05T15:00:00?lang=ru&units=si';
 
-      renderElement.innerHTML = `<div class="col-12">
+    fetch(`${proxy}/${API}/${key}/${params}`, {
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            // eslint-disable-next-line no-undef
+            const unixDate = moment.unix(response.currently.time);
+            const humanDate = unixDate.locale('ru').format('MMMM Do YYYY, h:mm:ss a');
+
+            renderElement.innerHTML = `<div class="col-12">
   Дата: ${humanDate}
 </div>
     <div class="col-12">
@@ -35,11 +40,11 @@ function getGrodnoForecast() {
     <div class="col-12">
       Видимость: ${response.currently.visibility}
     </div>`;
-      renderElement.classList.add('d-block');
-    });
+            renderElement.classList.add('d-block');
+        });
 }
 
 getButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  getGrodnoForecast();
+    event.preventDefault();
+    getGrodnoForecast();
 });
